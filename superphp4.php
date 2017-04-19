7ff568cf607ffa14da3d91d1d44779831d2c080c
Stian <stianStensli@users.noreply.github.com>
2017-04-19 22:52:44 +0200

    Update superphp4.php

--- superphp4.php
+++ superphp4.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["studentID"] ." email". $row["email"] ."<br>";
+        echo "id: " . $row["studentID"] .", email: ". $row["email"] ."<br>";
     }
 } else {
     echo "0 results";
