f3bf513d1cd8233d3991fe6c9c5eef936060ae34
Stian <stianStensli@users.noreply.github.com>
2017-04-19 22:20:46 +0200

    Update superphp3.php

--- superphp3.php
+++ superphp3.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('error_reporting', E_ALL);
 ?>
-
+<p>test</p>
 <?php
 $servername = "10.1.0.252";
 $username = "maxscaleuser";
@@ -13,4 +13,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+$sql = "show databases";
+$result = $conn->query($sql);
+echo "$result"
+    
+$conn->close();
 ?>
