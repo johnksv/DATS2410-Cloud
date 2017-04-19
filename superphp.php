4660e10cf9d49baf1edc561ce807e0aeda9e799c
Stenrud <truls.stenrud@gmail.com>
2017-04-19 12:52:45 +0200

    Update superphp.php

--- superphp.php
+++ superphp.php
@@ -8,8 +8,8 @@ ini_set('error_reporting', E_ALL);
 Testing php database
 <?php
 $servername = "10.1.0.252";
-$username = "testuser";
-$password = "password";
+$username = "";
+$password = "";
 $dbname = "testdb";
 
 // Create connection
