0a936f6a7aef16b096459d4d4510473f94e2d849
Stian <stianStensli@users.noreply.github.com>
2017-04-19 16:13:17 +0200

    Rename superphp.php to superphp2.php

---
+++ superphp2.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('error_reporting', E_ALL);
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+Testing php database directly:
+<?php
+$servername = "10.1.1.113";
+$username = "testuser";
+$password = "password";
+$dbname = "testdb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT studentID FROM Student";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<br> id: ". $row["studentID"]. "<br>";
+     }
+} else {
+     echo "0 results";
+}
+$conn->close();
+?>  
+
+</body>
+</html>
