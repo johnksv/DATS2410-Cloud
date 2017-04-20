0445db69082e06eec05f9d2cb5504b92a15e13d9
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-20 08:54:31 +0200

    test with select_db

--- superphp4.php
+++ superphp4.php
@@ -7,13 +7,13 @@ $servername = "10.1.0.252";
 $username = "maxscaleuser";
 $password = "placeSundayjudge";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, "testdb");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully. ";
-$conn->select_db("testdb");
+//$conn->select_db("testdb");
 
 $sql = "SELECT * FROM Student";
 $result = $conn->query($sql);
