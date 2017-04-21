acc73040186ef6dc44f7c4a22113a1fb0bf65766
Stenrud <truls.stenrud@gmail.com>
2017-04-21 10:43:23 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,37 @@
+<?php
+
+class Database
+{
+    private static $dbHost = '10.1.0.252' ;
+    private static $dbUsername = 'maxscaleuser';
+    private static $dbUserPassword = 'placeSundayJudge';
+
+    private static $cont  = null;
+
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+        // One connection through whole application
+        if ( null == self::$cont )
+        {
+            try
+            {
+                self::$cont =  new mysqli(self::$dbHost, self::$dbUsername, self::$dbUserPassword , "studentinfosys");
+            }
+            catch(PDOException $e)
+            {
+                die("Connection failed: " . self::$cont->connect_error);
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect()
+    {
+        self::$cont->close();
+    }
+}
+?>
