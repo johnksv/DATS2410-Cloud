7e1381f722827db816af996e6ea48d3366093627
Stenrud <truls.stenrud@gmail.com>
2017-04-21 11:50:02 +0200

    Update and rename database.php to connection.php

---
+++ connection.php
@@ -0,0 +1,35 @@
+<?php
+
+class Connection
+{
+    private static $conn  = null;
+    private static $host = '10.1.0.252' ;
+    private static $username = 'webserver';
+    private static $password = 'placeSundayjudge';
+    private static $database = 'studentinfosys';
+
+    public function construct() {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+        // One connection through whole application
+        if ( null == self::$conn )
+        {
+               self::$conn =  new mysqli(self::$host, self::$username, self::$password);
+               self::$conn->select_db(self::$database);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+        }
+        return self::$conn;
+    }
+
+    public static function disconnect()
+    {
+        self::$conn->close();
+        self::$conn = null;
+    }
+}
+?>
