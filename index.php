6f6abf0bdadd6c7c49bf206c656d4a001a057c0f
Stian <stianStensli@users.noreply.github.com>
2017-04-21 13:45:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ ini_set('error_reporting', E_ALL);
 ">
 	<h1>Group 08</h1>
 	<p>Private IP: <?php echo $_SERVER['SERVER_ADDR'] ?></p>
-	<p>Public IP: 128.39.121.59.8008</p>
+	<p>Public IP: 128.39.121.59:8008</p>
   </div>
 <canvas id="q" width="100%" height="1080">
 <script>var q=document.getElementById('q'),s=window.screen,w=q.width=s.width,h=q.height=s.height,p=Array(256).join(1).split(''),c=q.getContext("2d"),m=Math;setInterval(function(){c.fillStyle="rgba(0,0,0,0.05)";c.fillRect(0,0,w,h);c.fillStyle="rgba(0,255,0,1)";p=p.map(function(v,i){r=m.random();c.fillText(String.fromCharCode(m.floor(2720+r*33)),i*10,v);v+=10; return v>768+r*1e4?0:v})},33)</script>
