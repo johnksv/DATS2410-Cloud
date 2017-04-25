940db8929a5026d543ef206fcd9299dd5d3d8cc0
benjaminholsten <s305046@stud.hioa.no>
2017-04-25 13:43:41 +0200

    endret formatering student.php

--- show/student.php
+++ show/student.php
@@ -1,62 +1,66 @@
-<html>
-<body>
-
-<div class="container">
-        <div class="row">
-            <h3>Students</h3>
-        </div>
-       
-        <div class="row">
-            <table class="table table-striped table-bordered">
-                <thead>
-                <tr>
-                    
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Email</th>
-                    <th>StartYear</th>
-                    <th>StudentID</th>
-                    <th>  </th>
-                    <th>  </th>
-                </tr>
-                </thead>
-                <tbody>
 <?php
 require_once '../phpcode/Connection.php';
 $conn = Connection::connect();
 
 $sql = "SELECT * FROM Student";
 $result = $conn->query($sql);
- while($row = $result->fetch_assoc()) {
-        echo '<tr>';
-        echo '<td>'. $row['firstName'] . '</td>';
-        echo '<td>'. $row['lastName'] . '</td>';
-        echo '<td>'. $row['email'] . '</td>';
-        echo '<td>'. $row['startYear'] . '</td>';
-        echo '<td>'. $row['studentID'] . '</td>';
-        echo '<td>' . 
-            '<form action="../update/student.php" method="post">
-            <input type="hidden" name="studentID" value="' . $row['studentID'] . '">
-            <input type="submit"  name="Change" value="Edit"><br>
-            
-        </form>'. '</td>';
-        echo '<td>' . 
-            '<form action="delete.php" method="post">
-            <input type="hidden" name="id" value="' . $row['studentID'] . '">
-            <input type="submit"  name="Delete" value="Delete"><br>
-            
-        </form>'. '</td>';
-        
-        echo '</tr>';
-    }
-
-$conn->close();
 ?>
-   </tbody>
-            </table>
+<html>
+    <head>
+        
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <h3>Students</h3>
+            </div>
+
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                            <th>StartYear</th>
+                            <th>StudentID</th>
+                            <th>  </th>
+                            <th>  </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = $result->fetch_assoc()) { ?>
+                            <tr>
+                                <td><?php echo $row['firstName'] ?></td>
+                                <td><?php echo $row['lastName'] ?></td>
+                                <td><?php echo $row['email'] ?></td>
+                                <td><?php echo $row['startYear'] ?></td>
+                                <td><?php echo $row['studentID'] ?></td>
+                                <td> 
+                                    <form action="../update/student.php" method="post">
+                                        <input type="hidden" name="studentID" value="<?php echo $row['studentID'] ?>">
+                                        <input type="submit"  name="Change" value="Edit"><br>
+
+                                    </form>
+                                </td>
+                                <td> 
+                                    <form action="delete.php" method="post">
+                                        <input type="hidden" name="id" value="<?php echo $row['studentID'] ?>">
+                                        <input type="submit"  name="Delete" value="Delete"><br>
+
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php }
+                        $conn->close();
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
 
 
