510efb58a3b9f7a7dbb45b0c5ea8b32f9a2458eb
benjaminholsten <s305046@stud.hioa.no>
2017-04-25 14:52:49 +0200

    fixed deleting in delete.php

--- show/delete.php
+++ show/delete.php
@@ -4,13 +4,13 @@ require_once '../phpcode/Connection.php';
 $conn = Connection::connect();
 
 $type = filter_input(INPUT_POST, "type");
-if (strcmp($type, "student")) {
+if (strcmp($type, "student") == 0) {
     $sql = "DELETE FROM Student WHERE studentID='" . $_POST["id"] . "' ";
     if ($conn->query($sql) === TRUE) {
         Connection::disconnect();
         header('Location: student.php');
     }
-} else if (strcmp($type, "course")) {
+} else if (strcmp($type, "course") == 0) {
     $sql = "DELETE FROM Course WHERE courseCode='" . $_POST["id"] . "' ";
     if ($conn->query($sql) === TRUE) {
         Connection::disconnect();
