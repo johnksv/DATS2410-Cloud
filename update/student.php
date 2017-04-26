c3392ed24c10f61944ac4a76ea19ac2e3de0c8c2
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-26 10:49:00 +0200

    Updated student.php

--- update/student.php
+++ update/student.php
@@ -1,71 +1,86 @@
-<!DOCTYPE HTML>
+<?php
+ini_set('error_reporting', E_ALL);
+require '../Connection.php';
+$fname = null;
+$lname = null;
+$email = null;
+$year = null;
+$conn = Connection::connect();
+
+if (!empty($_POST["update"])) {
+
+    $sql = "UPDATE Student SET email='" . $_POST["email"] .
+        "', startYear='" . $_POST['startYear'] .
+        "', firstName='" . $_POST['firstName'] .
+        "', lastName='" . $_POST['lastName'] .
+        "' WHERE studentId='" . $_POST["foo"] .
+        "'";
+
+    $result = $conn->query($sql);
+    Connection::disconnect();
+    header("Location: ../show/student.php");
+}
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>HiOA student information system</title>
 </head>
-	<body>
-	<?php
-	ini_set('error_reporting', E_ALL);
-	//require_once 'phpcode/connection.php';
-	?>
-	<?php
-	require '../Connection.php';
-	$fname = null;
-	$lname = null;
-	$email = null;
-	$year  = null;
-	$conn = Connection::connect();
+<body>
+<header>
+    <nav>
+        <a>
+            Home
+        </a>
+        <a>
+            Students
+        </a>
+        <a>
+            Courses
+        </a>
+        <a>
+            Exams
+        </a>
+    </nav>
+</header>
 
-	if(!empty($_POST["update"])){
+<?php
+//If the request is from another webpage
+if (empty($_POST["studentID"])) {
+    //If direct access
+    header("Location: ../show/student.php");
+} else {
+    echo "<B>Updating student: " . $_POST["studentID"] . "</B><br>";
 
-		$sql = "UPDATE Student SET email='". $_POST["email"].
-            "', startYear='" . $_POST['startYear'].
-            "', firstName='". $_POST['firstName'].
-            "', lastName='" . $_POST['lastName'] .
-            "' WHERE studentId='" . $_POST["foo"].
-            "'";
+    $sql = 'SELECT * FROM Student WHERE studentID="' . $_POST["studentID"] . '"';
+    $result = $conn->query($sql);
+    Connection::disconnect();
 
-        $result = $conn->query($sql);
-        Connection::disconnect();
-        header("Location: ../show/student.php");
-	}
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $fname = $row['firstName'];
+            $lname = $row['lastName'];
+            $email = $row['email'];
+            $year = $row['startYear'];
+        }
+    } else {
+        echo "No such user!";
+    }
+}
+?>
+<form action="student.php" method="post">
+    First Name: <input type="text" name="firstName" value="<?php echo $fname; ?>"><br>
+    Last Name: <input type="text" name="lastName" value="<?php echo $lname; ?>"><br>
+    email: <input type="text" name="email" value="<?php echo $email; ?>"><br>
 
-	//If the request is from another webpage
-	if (empty($_POST["studentID"])) {
-		echo "EMPTY";
-	}else{
-		echo "<B>Updating student: ". $_POST["studentID"] ."</B><br>";
+    Start Year: <input name="startYear" type="date" value="<?php echo $year; ?>"><br>
+    <br>
+    <input type="hidden" name="foo" value="<?php echo $_POST["studentID"]; ?>"/>
+    <input type="submit" name="update" Value="Update">
+</form>
 
-		$sql = 'SELECT * FROM Student WHERE studentID="'.$_POST["studentID"] . '"';
-		$result = $conn->query($sql);
-
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while ($row = $result->fetch_assoc()) {
-				$fname = $row['firstName'];
-				$lname = $row['lastName'];
-				$email = $row['email'];
-				$year = $row['startYear'];
-			}
-
-		} else {
-			echo "No such user!";
-		}
-
-		Connection::disconnect();
-	}
-	?>
-	<form action="student.php" method="post">
-		First Name: <input type="text" name="firstName" value="<?php echo $fname; ?>"><br>
-		Last Name: <input type="text" name="lastName" value="<?php echo $lname; ?>"><br>
-		email: <input type="text" name="email" value="<?php echo $email; ?>"><br>
-
-		Start Year: <input name="startYear" type="date" value="<?php echo $year; ?>"><br>
-		<br>
-		<input type="hidden" name="foo" value="<?php echo $_POST["studentID"];?>" />
-		<input type="submit" name="update" Value="Update">
-	</form>
-
-	</body>
+</body>
 </html>
