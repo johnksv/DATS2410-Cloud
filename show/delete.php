7e31f88d78eda6d08eeb6d063b4c8fc83a7bf0bb
benjaminholsten <s305046@stud.hioa.no>
2017-04-26 09:48:00 +0200

    delete update

--- show/delete.php
+++ show/delete.php
@@ -15,22 +15,8 @@ if (strcmp($type, "student") == 0) {
     if ($conn->query($sql) === TRUE) {
         connection::disconnect();
         header('Location: course.php');
-    }
-    
+    }    
 }
-<<<<<<< HEAD
-
-=======
-else if (strcmp($type, "electivecourse") == 0) {
-    $sql = "DELETE FROM Course WHERE sPID='" . $_POST["id"] . "' ";
-    if ($conn->query($sql) === TRUE) {
-        connection::disconnect();
-        header('Location: electivecourse.php');
-    }
-    
-}
->>>>>>> 602d25ab87a23ca757913923db4a4cfe304bebda
-
 echo "Error deleting record: " . $conn->error;
 
 connection::disconnect();
