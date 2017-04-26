5533038507297574ed241f675c5f0120a2062c60
benjaminholsten <s305046@stud.hioa.no>
2017-04-26 13:00:05 +0200

    changes in deleteInfo

---
+++ show/deleteInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../Connection.php';
+$conn = Connection::connect();
+$sql = "DELETE FROM ElectiveCourse WHERE sPID='" . $_POST["id"] . "' and courseCode='" . $_POST["course"] . "'";
+    if ($conn->query($sql) === TRUE) {
+        Connection::disconnect();
+        header('Location: studyprogramInfo.php');
+
+    }
+        
