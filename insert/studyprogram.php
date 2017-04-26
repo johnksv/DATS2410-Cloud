d74fc865552d07a91731aa808bcb61b8e259b22a
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-26 10:39:02 +0200

    Updated studyprogram.php

--- insert/studyprogram.php
+++ insert/studyprogram.php
@@ -1,35 +1,28 @@
 <?php
 
 require '../Connection.php';
-die('Not implemented yet..!');
 if (!empty($_POST)) {
     // keep track validation errors
     $IDError = null;
-    $emailError = null;
+    $programNameError = null;
     $yearError = null;
-    $firstNameError = null;
-    $lastNameError = null;
+    $durationError = null;
 
     // keep track post values
-    $studentID = $_POST['studentID'];
-    $email = $_POST['email'];
+    $spid = $_POST['sPID'];
+    $spname = $_POST['sPName'];
+    $durationSemester = $_POST['durationSemester'];
     $startYear = $_POST['startYear'];
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-
 
     // validate input
     $valid = true;
-    if (empty($studentID)) {
-        $IDError = 'Please enter student ID';
+    if (empty($spid)) {
+        $IDError = 'Please enter a ID';
         $valid = false;
     }
 
-    if (empty($email)) {
-        $emailError = 'Please enter Email Address';
-        $valid = false;
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailError = 'Please enter a valid Email Address';
+    if (empty($spname)) {
+        $programNameError = 'Please enter Program Long Name';
         $valid = false;
     }
 
@@ -39,90 +32,81 @@ if (!empty($_POST)) {
         return $d && $d->format('Y-m-d') === $date;
     }
 
+    if (empty($durationSemester)) {
+        $yearError = 'Please enter duration';
+        $valid = false;
+    }
+
     if (empty($startYear)) {
-        $yearError = 'Please enter date';
-        $valid = false;
-    }
-
-    if (empty($firstName)) {
-        $firstNameError = 'Please enter Name';
-        $valid = false;
-    }
-
-    if (empty($lastName)) {
-        $lastNameError = 'Please enter Mobile Number';
+        $durationError = 'Please enter start year';
         $valid = false;
     }
 
     // insert data
     if ($valid) {
         $conn = Connection::connect();
-        $sql = "INSERT INTO Studyprogram ('sPID', 'sPName', 'durationSemester', 'startYear') values('$studentID', '$email', '$startYear', '$firstName', '$lastName')";
+        $sql = "INSERT INTO Studyprogram ('sPID', 'sPName', 'durationSemester', 'startYear') values('$spid', '$spname', '$durationSemester', '$startYear')";
 
         $result = $conn->query($sql);
         Connection::disconnect();
-        header("Location: ../show/student.php");
+        header("Location: ../show/studyprogram.php");
     }
 }
 ?>
 
-<!DCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
 </head>
 <body>
 
-<div class="span10 offset1">
+<div>
 
-        <h3>Create a Student</h3>
+    <h3>Create Student Program</h3>
 
     <form action="student.php" method="post">
-	     
-	  <label>Student ID</label>
-	  <div> 
-            <input name="studentID" type="text" placeholder="sxxxxxx" value="<?php echo !empty($studentID) ? $studentID : ''; ?>">
+
+        <label>Program ID</label>
+        <div>
+            <input name="sPID" type="text" placeholder="sxxxxxx" value="
+            <?php echo !empty($spid) ? $spid : ''; ?>">
             <?php if (!empty($IDError)): ?>
                 <span><?php echo $IDError; ?></span>
             <?php endif; ?>
-	 </div>
-<label>Email</label>
-<div>
-    <input name="email" type="text" placeholder="my.example@example.com" value="<?php echo !empty($email) ? $email : ''; ?>">
-    <?php if (!empty($emailError)): ?>
-        <span><?php echo $emailError; ?></span>
-    <?php endif; ?>
-</div>
+        </div>
+        <label>Program long name</label>
+        <div>
+            <input name="sPName" type="text" placeholder="my.example@example.com"
+                   value="<?php echo !empty($spname) ? $spname : ''; ?>">
+            <?php if (!empty($programNameError)): ?>
+                <span><?php echo $programNameError; ?></span>
+            <?php endif; ?>
+        </div>
 
-<label>Start year</label>
-<div>
-    <input name="startYear" type="date" placeholder="yyyy-mm-dd" value="<?php echo !empty($startYear) ? $startYear : ''; ?>">
-    <?php if (!empty($yearError)): ?>
-        <span><?php echo $yearError; ?></span>
-    <?php endif; ?>
-</div>
+        <label>Duration (number of semesters)</label>
+        <div>
+            <input name="durationSemester" type="text" placeholder="First Name"
+                   value="<?php echo !empty($startYear) ? $startYear : ''; ?>">
+            <?php if (!empty($durationError)): ?>
+                <span><?php echo $durationError; ?></span>
+            <?php endif; ?>
+        </div>
 
-<label>First Name</label>
-<div>
-    <input name="firstName" type="text" placeholder="First Name" value="<?php echo !empty($firstName) ? $firstName : ''; ?>">
-    <?php if (!empty($firstNameError)): ?>
-        <span><?php echo $firstNameError; ?></span>
-    <?php endif; ?>
-</div>
+        <label>Start year</label>
+        <div>
+            <input name="startYear" type="date" placeholder="yyyy-mm-dd"
+                   value="<?php echo !empty($durationSemester) ? $durationSemester : ''; ?>">
+            <?php if (!empty($yearError)): ?>
+                <span><?php echo $yearError; ?></span>
+            <?php endif; ?>
+        </div>
 
-<label>Last Name</label>
-<div>
-    <input name="lastName" type="text" placeholder="Last Name" value="<?php echo !empty($lastName) ? $lastName : ''; ?>">
-    <?php if (!empty($lastNameError)): ?>
-        <span><?php echo $lastNameError; ?></span>
-    <?php endif; ?>
-</div>
-
-<div>
-    <button type="submit">Create</button>
-    <a href="../show/student.php">Back</a>
-</div>
-</form>
+        <div>
+            <button type="submit">Create</button>
+            <a href="../show/studyprogram.php">Back</a>
+        </div>
+    </form>
 </div>
 
 
