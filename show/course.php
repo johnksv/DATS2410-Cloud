11ff7ea7e08f591bdd549245e2f64a5f66229cff
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-27 10:20:35 +0200

    Updated table to div

--- show/course.php
+++ show/course.php
@@ -9,7 +9,7 @@ Connection::disconnect();
 <!DOCTYPE html>
 <html>
 <head>
-    <?php readfile("../htmlTemplate/head.html");  ?>
+    <?php readfile("../htmlTemplate/head.html"); ?>
 </head>
 <body>
 
@@ -20,47 +20,42 @@ readfile("../htmlTemplate/header.html");
 <main>
     <div>
         <h3>Courses</h3>
-        <a href="../insert/course.php"><button>Create new entry</button></a>
+        <a href="../insert/course.php">
+            <button>Create new entry</button>
+        </a>
     </div>
 
-    <div>
-        <table>
-            <thead>
-            <tr>
 
-                <th>Course code</th>
-                <th>Course title</th>
-                <th>Semester</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
+        <div class="table">
+            <div class="row table-header">
+                <div>Course code</div>
+                <div>Course title</div>
+                <div>Semester</div>
+            </div>
+
             <?php while ($row = $result->fetch_assoc()) { ?>
-                <tr>
-                    <td><?php echo $row['courseCode'] ?></td>
-                    <td><?php echo $row['courseTitle'] ?></td>
-                    <td><?php echo $row['semester'] ?></td>
-                    <td>
+                <div class="row">
+                    <div><?php echo $row['courseCode'] ?></div>
+                    <div><?php echo $row['courseTitle'] ?></div>
+                    <div><?php echo $row['semester'] ?></div>
+                    <div>
                         <form action="../update/course.php" method="post">
                             <input type="hidden" name="courseCode" value="<?php echo $row['courseCode'] ?>">
                             <input type="submit" name="Change" value="Edit"><br>
 
                         </form>
-                    </td>
-                    <td>
+
                         <form action="delete.php" method="post">
                             <input type="hidden" name="id" value="<?php echo $row['courseCode'] ?>">
                             <input type="hidden" name="type" value="course">
                             <input type="submit" name="Delete" value="Delete"><br>
 
                         </form>
-                    </td>
-                </tr>
+                    </div>
+                </div>
             <?php } ?>
-            </tbody>
-        </table>
-    </div>
+
+        </div>
 </main>
 
 <?php include '../htmlTemplate/footer.php'; ?>
