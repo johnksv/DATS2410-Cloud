8e96d2b21562f1417946e65a144ed3d5810f8c0b
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-28 12:37:41 +0200

    Active site is highlighted

--- htmlTemplate/header.php
+++ htmlTemplate/header.php
@@ -1,15 +1,20 @@
 <?php
 
-    $site = $_SERVER['REQUEST_URI'];
-    echo $site;
+$site = $_SERVER['REQUEST_URI'];
+echo $site;
+$student = "/show/student.php";
+$course = "/show/course.php";
+$program = "/show/studyprogram.php";
+
+
 ?>
 
 
 <header>
     <nav>
         <a href="/">Home</a>
-        <a href="/show/student.php">Students</a>
-        <a href="/show/course.php">Courses</a>
-        <a href="/show/studyprogram.php">Programs</a>
+        <a <?php if ($site === $student) { ?> class="active" <?php } ?> href="<?php echo $student; ?>">Students</a>
+        <a <?php if ($site === $course) { ?> class="active" <?php } ?> href="<?php echo $course; ?>">Courses</a>
+        <a <?php if ($site === $program){ ?> class="active" <?php } ?>href="<?php echo $program; ?>">Programs</a>
     </nav>
 </header>
