6c261f14a81898d8b89a677c5310278d5d2159b0
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-28 17:53:07 +0200

    changed delete back to direct redirection

--- show/delete.php
+++ show/delete.php
@@ -14,39 +14,39 @@ if (strcmp($type, "student") == 0) {
     $sql = "DELETE FROM Student WHERE studentID='$id' ";
     if ($conn->query($sql) === TRUE) {
         $conn->close();
-        header("Refresh: 1;URL=student.php");
+        header("Location: student.php");
     }
 } else if (strcmp($type, "course") == 0) {
     $sql = "DELETE FROM Course WHERE courseCode='$id' ";
     if ($conn->query($sql) === TRUE) {
         $conn->close();
-        header("Refresh: 1;URL=course.php");
+        header("Location: course.php");
     }
 } else if (strcmp($type, "studyprogram") == 0) {
     $sql = "DELETE FROM StudyProgram WHERE sPID='$id' ";
     if ($conn->query($sql) === TRUE) {
         $conn->close();
-        header("Refresh: 1;URL=studyprogram.php");
+        header("Location: studyprogram.php");
     }
 } else if (strcmp($type, "studyProgram") == 0) {
     if (strcmp($type, "elective") == 0) {
         $sql = "DELETE FROM ElectiveCourse WHERE sPID='$id' AND courseCode='$course'";
         if ($conn->query($sql) === TRUE) {
             $conn->close();
-            header("Refresh: 1;URL=studyprograminfo.php?id=$course");
+            header("Location: studyprograminfo.php?id=$course");
         }
     } else if (strcmp($type, "mandatory") == 0) {
         $sql = "DELETE FROM MandatoryCourse WHERE sPID='$id' AND courseCode='$course'";
         if ($conn->query($sql) === TRUE) {
             $conn->close();
-            header("Refresh: 1;URL=studyprograminfo.php?id=$course");
+            header("Location: studyprograminfo.php?id=$course");
         }
     }
 } else if (strcmp($type, "course") == 0) {
     $sql = "DELETE FROM Course_Instance WHERE startDate='$id' AND courseCode='$course'";
     if ($conn->query($sql) === TRUE) {
         $conn->close();
-        header("Refresh: 1;URL=courseinfo.php?id=$course");
+        header("Location: courseinfo.php?id=$course");
     }
 
 } else {
