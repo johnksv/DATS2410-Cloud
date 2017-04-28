3aa6af429f1b5f0f0cb4b70e79135737c8058372
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-28 12:40:58 +0200

    updated a-tag such that they don't get space

--- htmlTemplate/header.php
+++ htmlTemplate/header.php
@@ -1,7 +1,7 @@
 <?php
 
 $site = $_SERVER['REQUEST_URI'];
-echo $site;
+
 $student = "/show/student.php";
 $course = "/show/course.php";
 $program = "/show/studyprogram.php";
@@ -12,9 +12,10 @@ $program = "/show/studyprogram.php";
 
 <header>
     <nav>
-        <a href="/">Home</a>
-        <a <?php if ($site === $student) { ?> class="active" <?php } ?> href="<?php echo $student; ?>">Students</a>
-        <a <?php if ($site === $course) { ?> class="active" <?php } ?> href="<?php echo $course; ?>">Courses</a>
-        <a <?php if ($site === $program){ ?> class="active" <?php } ?>href="<?php echo $program; ?>">Programs</a>
+        <!-- Need this ugly formating to render correctly without padding between the tags -->
+        <a href="/">Home</a><a <?php if ($site === $student) { ?> class="active" <?php } ?>
+                href="<?php echo $student; ?>">Students</a><a <?php if ($site === $course) { ?> class="active" <?php } ?>
+                href="<?php echo $course; ?>">Courses</a><a <?php if ($site === $program){ ?> class="active"
+                                                                                              <?php } ?>href="<?php echo $program; ?>">Programs</a>
     </nav>
 </header>
