67a3b2ba4dcf2f30372945772942f3231f17dda3
Stian Stensli <stian.hs@hotmail.com>
2017-04-28 14:50:55 +0200

    test

--- index.php
+++ index.php
@@ -8,7 +8,41 @@
 </head>
 <body>
 
+<?php
+require_once 'Connection.php';
+$footerConn = (new Connection())->connect();
+$dbHostInfo = $footerConn->host_info;
+$footerConn->close();
 
+$datetime = date("j F, Y, H:i");
+
+$site = $_SERVER['REQUEST_URI'];
+
+$student = "/show/student.php";
+$course = "/show/course.php";
+$program = "/show/studyprogram.php";
+
+
+?>
+
+
+<header>
+    <nav>
+        <a href="/">Home</a><a <?php if ($site === $student) { ?> class="active" <?php } ?>
+                href="<?php echo $student; ?>">Students</a><a <?php if ($site === $course) { ?> class="active" <?php } ?>
+                href="<?php echo $course; ?>">Courses</a><a <?php if ($site === $program){ ?> class="active"
+                                                                                              <?php } ?>href="<?php echo $program; ?>">Programs</a>
+    </nav>
+
+    <div>
+        <p>You are served by the server IP: <b><?php echo $_SERVER['SERVER_ADDR']; ?> </b></p>
+        <p>Database IP: <b><?php echo "$dbHostInfo" ?> </b></p>
+
+        <p> On: <b> <?php echo "$datetime" ?></b></p>
+
+        <p>Group 8</p>
+    </div>
+</header>
 <main>
 	<h1>
 		Oblig 3 Group 8
