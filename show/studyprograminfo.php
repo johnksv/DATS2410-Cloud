46c8f9459f0576bbe9a3985c93bf64807ae56f47
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-29 12:09:02 +0200

    Updated delete

--- show/studyprograminfo.php
+++ show/studyprograminfo.php
@@ -59,7 +59,7 @@ include_once '../html/header.php';
                             <input type="hidden" name="id" value="<?php echo $row['sPID'] ?>">
                             <input type="hidden" name="course" value="<?php echo $row['courseCode'] ?>">
                             <input type="hidden" name="courseInfo" value="elective">
-                            <input type="hidden" name="type" value="studyProgram">
+                            <input type="hidden" name="type" value="studyprograminfo">
                             <input type="submit" name="Delete" value="Delete"><br>
 
                         </form>
