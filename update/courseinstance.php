209adc74507d2edbc3a14b556078e67af606b6e5
John Kasper Svergja <john.k.svergja@gmail.com>
2017-04-30 11:39:54 +0200

    sql prepare in courseinstance

--- update/courseinstance.php
+++ update/courseinstance.php
@@ -1,24 +1,27 @@
 <?php
 require_once '../Connection.php';
 
-$conn = (new Connection())->connect();
-
 if (!empty($_POST["update"])) {
 
-    $courseCode = $_POST["courseCode"];
-    $startDate = $_POST["startDate"];
-    $examDate = $_POST["examDate"];
-    $sql = "UPDATE Course_Instance SET examDate='$examDate' WHERE courseCode='$courseCode' and startDate='$startDate'";
+    $courseCode = filter_input(INPUT_POST, "courseCode");
+    $startDate = filter_input(INPUT_POST, "startDate");
+    $examDate = filter_input(INPUT_POST, "examDate");
 
-    $result = $conn->query($sql);
+    $conn = (new Connection())->connect();
+    $stat = $conn->prepare("UPDATE Course_Instance SET examDate=? WHERE courseCode=? and startDate=?");
+    $stat->bind_param("sss", $examDate, $courseCode, $startDate);
+    $stat->execute();
+    $result = $stat->get_result();
     $conn->close();
-    header("Location: ../show/courseinfo.php?id=".$_POST["courseCode"]);
+
+
+    header("Location: ../show/courseinfo.php?id=" . $_POST["courseCode"]);
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <?php readfile("../html/head.html");  ?>
+    <?php readfile("../html/head.html"); ?>
 </head>
 <body>
 
@@ -29,48 +32,49 @@ include_once '../html/header.php';
 
 <main>
     <div class="innerMain">
-    <?php
-    //If the request is from another webpage
-    if (empty($_POST["courseCode"])) {
-        echo "<h1>Direct access not allowed, redirecting</h1>";
-        header('Refresh: 2;URL=../show/course.php');
-    } else {
-        echo "<B>Updating exam date for " . $_POST["courseCode"] . " starting at ". $_POST["startDate"]."</B><br>";
-
-        $courseCode = $_POST["courseCode"];
-        $startDate = $_POST["startDate"];
-
-        $sql = "SELECT * FROM Course_Instance WHERE courseCode='$courseCode' and startDate='$startDate'";
-
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                $courseCode = $row['courseCode'];
-                $startDate = $row['startDate'];
-                $examDate = $row['examDate'];
-            }
-
+        <?php
+        //If the request is from another webpage
+        if (empty($_POST["courseCode"])) {
+            echo "<h1>Direct access not allowed, redirecting</h1>";
+            header('Refresh: 2;URL=../show/course.php');
         } else {
-            echo "No such course!";
-        }
+            $courseCode = filter_input(INPUT_POST, "courseCode");
+            $startDate = filter_input(INPUT_POST, "startDate");
+            echo "<b>Updating exam date for $courseCode starting at $startDate </b><br>";
 
-        $conn->close(); ?>
+            $conn = (new Connection())->connect();
+            $stat = $conn->prepare("SELECT * FROM Course_Instance WHERE courseCode=? and startDate=?");
+            $stat->bind_param("ss", $courseCode, $startDate);
+            $stat->execute();
+            $result = $stat->get_result();
+            $conn->close();
 
-        <form action="courseinstance.php" method="post">
-            Exam date: <input type="date" name="examDate" value="<?php echo $examDate; ?>"><br>
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    $courseCode = $row['courseCode'];
+                    $startDate = $row['startDate'];
+                    $examDate = $row['examDate'];
+                }
+            } else {
+                echo "<h1>No such course in database, redirecting</h1>";
+                header('Refresh: 2;URL=../show/course.php');
+            } ?>
+
+            <form action="courseinstance.php" method="post">
+                Exam date: <input type="date" name="examDate" value="<?php echo $examDate; ?>"><br>
 
 
+                <br>
 
-            <br>
+                <input type="hidden" name="startDate" value="<?php echo $startDate; ?>"/>
+                <input type="hidden" name="courseCode" value="<?php echo $courseCode; ?>"/>
+                <input type="submit" name="update" Value="Update">
+            </form>
 
-            <input type="hidden" name="startDate" value="<?php echo $startDate; ?>"/>
-            <input type="hidden" name="courseCode" value="<?php echo $courseCode; ?>"/>
-            <input type="submit" name="update" Value="Update">
-        </form>
-
-    <?php } ?>
+        <?php } ?>
     </div>
 </main>
 
